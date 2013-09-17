b479ab1ec3147f7d5dfcda47086f2b25560dbb1f
Jeroen <jeroen@wlan-222078.nbw.tue.nl>
2013-09-17 11:37:12 +0200

    Storage test iteration 3 - error on reading db

--- www/js/storage.js
+++ www/js/storage.js
@@ -4,24 +4,50 @@
 //	iPhone
 //source: http://docs.phonegap.com/en/1.2.0/phonegap_storage_storage.md.html
 
-$(function() {
 
-	function populateDB(tx) {
-	 tx.executeSql('DROP TABLE IF EXISTS DEMO');
-	 tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
-	 tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
-	 tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
-	}
+onDeviceReady = function () {
+	alert('device ready');
+	console.log('OPVALLEND!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+	storageTest();
+};
+document.addEventListener("deviceready", onDeviceReady, false);
+
 	
-	function errorCB(err) {
-	    alert("Error processing SQL: "+err.code);
-	}
-	
-	function successCB() {
-	    alert("success!");
-	}
-	
+function storageTest() {
+
 	var db = window.openDatabase("Database", "1.0", "PhoneGap Demo", 200000);
 	db.transaction(populateDB, errorCB, successCB);
-});
+	
+	alert('DB Filled');
+	db.transaction(queryDB, errorCB);
+}
 
+
+function populateDB(tx) {
+	tx.executeSql('DROP TABLE IF EXISTS DEMO');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
+	tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
+	tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+}
+
+function errorCB(err) {
+	alert("Error processing SQL: "+err.code);
+}
+
+function successCB() {
+	alert("success!");
+}
+
+
+function queryDB(tx) {
+	tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
+}
+
+function querySuccess(tx, results) {
+	// this will be empty since no rows were inserted.
+	console.log("Insert ID = " + results.insertId); //TODO: Error here in android (adb logcat | grep 'Web Console')
+	// this will be 0 since it is a select statement
+	console.log("Rows Affected = " + results.rowAffected);
+	// the number of rows returned by the select statement
+	console.log("Insert ID = " + results.rows.length);
+}
