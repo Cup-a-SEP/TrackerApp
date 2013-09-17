49955943b1ba8f4005a58456bffac0593d4854ff
Ferry Timmers <ferry.timmers@student.tue.nl>
2013-09-17 11:43:33 +0200

    refactored geocoding

--- www/geocode.js
+++ www/geocode.js
@@ -1,31 +1,82 @@
+var Geo = {};
+
+Geo.ServerPath = 'http://maps.googleapis.com/maps/api/geocode/json';
+
 /**
 * Converts an human readable location to geo-coordinates
 */
-function Geocode(address, callback)
+Geo.code = function Geocode(address)
 {
-	$.getJSON('http://maps.googleapis.com/maps/api/geocode/json',
+	var def = $.Deferred();
+	
+	$.getJSON(Geo.ServerPath,
 	{
 		address: address,
-		sensor: true
-	}, function(data)
+		sensor: true,
+		region: 'nl'
+	}).done(function(data)
 	{
-		if (data.results[0])
-			callback(data.results[0].geometry.location);
+		if (data.status == 'OK')
+		{
+			data.results = Geo.filterCountry(data.results, 'NL');
+			if (data.results.length < 1)
+				def.reject('ZERO_RESULTS');
+			else
+			{
+				var addresses = data.results[0].address_components;
+				addresses = $.map(addresses, function(x) { return x.long_name; });
+				def.resolve(data.results[0].geometry.location, addresses);
+			}
+		}
+		else
+			def.reject(data.status);
+	}).fail(function(jqxhr, textStatus, error)
+	{
+		def.reject(textStatus);
 	});
-}
+	return def;
+};
 
 /**
 * Converts geo-coordinates to an human readable location
 */
-function Geodecode(latlng, callback)
+Geo.decode = function Geodecode(latlng)
 {
-	$.getJSON('http://maps.googleapis.com/maps/api/geocode/json',
+	var def = $.Deferred();
+	
+	$.getJSON(Geo.ServerPath,
 	{
-		latlng: latlng.lat + ',' + latlng.lng,
+		latlng: latlng,
 		sensor: true
-	}, function(data)
+	}).done(function(data)
 	{
-		if (data.results[0])
-			callback(data.results[0].formatted_address);
+		if (data.status == 'OK')
+		{
+			var addresses = data.results[0].address_components;
+			addresses = $.map(addresses, function(x) { return x.long_name; });
+			def.resolve(data.results[0].geometry.location, addresses);
+		}
+		else
+			def.reject(data.status);
+	}).fail(function(jqxhr, textStatus, error)
+	{
+		def.reject(textStatus);
 	});
-}
+	
+	return def;
+};
+
+/**
+*
+*/
+Geo.filterCountry = function(result, country)
+{
+	return $.grep(result, function(x)
+	{
+		return $.map(x.address_components, function(y)
+		{
+			if ($.inArray('country',y.types) >= 0)
+				return y.short_name;
+		})[0] == country;
+	});
+};
