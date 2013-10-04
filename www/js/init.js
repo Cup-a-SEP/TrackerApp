39394c4c6ba3d5284cf2dc2032c99223292a2798
Ferry Timmers <f.timmers@student.tue.nl>
2013-10-04 16:36:19 +0200

    Alarm

--- www/js/init.js
+++ www/js/init.js
@@ -16,6 +16,8 @@ function onBackButton()
 	history.back();
 }
 
+var nextOTP;
+var lastAlarm;
 
 $(function()
 {
@@ -23,4 +25,42 @@ $(function()
 	{
 		onBackButton();
 	});
+	
+	Storage.init();
+	
+	// debug:
+	localStorage['Alarm departure setting'] = 'true';
+	localStorage['Alarm departure time'] = '10';
+	
+	Storage.Trips.init();
+	setTimeout(startPolling, 1000);
 });
+
+function startPolling()
+{
+	Storage.Trips.next().done(function(req)
+	{
+		console.log(req);
+		OTP.plan(req).done(function(data)
+		{
+			console.log(data);
+			nextOTP = data;
+			setInterval(polling, 1000);
+		});
+	});
+}
+
+function polling()
+{
+	var now = new Date().getTime();
+	var alarm = nextOTP.itineraries[0].startTime - localStorage['Alarm departure time'];
+	
+	if (localStorage['Alarm departure setting'] == 'true')
+	{
+		if (alarm != lastAlarm && alarm < now)
+		{
+			alert('Het is tijd!!');
+			lastAlarm = alarm;
+		}
+	} 
+}
