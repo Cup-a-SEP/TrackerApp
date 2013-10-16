9a1764278ccdb5409d067e32f38d1551d808b82c
Ferry Timmers <f.timmers@student.tue.nl>
2013-10-16 11:16:58 +0200

    Settings ajaxified

---
+++ www/js/page/settings.js
@@ -0,0 +1,71 @@
+/**
+ * Settings page namespace
+ * @namespace Page.Settings 
+ */
+Page.Settings = {};
+
+/**
+ * Initializes settings page interface
+ */
+Page.Settings.init = function PageSettingsInit()
+{	
+	$('#departure-slider').val(localStorage['Alarm departure time']).slider();
+	$('#embark-slider').val(localStorage['Alarm embark time']).slider();
+	$('#alight-slider').val(localStorage['Alarm alight time']).slider();
+	
+	$('#departure-' + (localStorage['Alarm departure setting'] == 'true' ? 'on' : 'off')).prop('checked',true);//.checkboxradio();
+	$('#embark-' + (localStorage['Alarm embark setting'] == 'true' ? 'on' : 'off')).prop('checked',true);//.checkboxradio();
+	$('#alight-' + (localStorage['Alarm alight setting'] == 'true' ? 'on' : 'off')).prop('checked',true);//.checkboxradio();
+	
+	$('fieldset').buttonset();
+	
+	$('#departure-slider').change(function() {
+		setTimeout(function() {
+			localStorage['Alarm departure time'] = $('#departure-slider').val();
+			Service.Alarm.refresh();
+		},0);
+	});
+	$('#embark-slider').change(function() {
+		setTimeout(function() {
+			localStorage['Alarm embark time'] = $('#embark-slider').val();
+			Service.Alarm.refresh();
+		},0);
+	});
+	$('#alight-slider').change(function() {
+		setTimeout(function() {
+			localStorage['Alarm alight time'] = $('#alight-slider').val();
+			Service.Alarm.refresh();
+		},0);
+	});
+	
+	$('.departure-control').click(function() {
+		setTimeout(function() {
+			localStorage['Alarm departure setting'] = $('#departure-on').prop('checked');
+			Service.Alarm.refresh();
+		},0);
+	});
+	
+	$('.embark-control').click(function() {
+		setTimeout(function() {
+			localStorage['Alarm embark setting'] = $('#embark-on').prop('checked');
+			Service.Alarm.refresh();
+		},0);
+	});
+	
+	$('.alight-control').click(function() {
+		setTimeout(function() {
+			localStorage['Alarm alight setting'] = $('#alight-on').prop('checked');
+			Service.Alarm.refresh();
+		},0);
+	});
+	
+	// naam waarde
+	/*
+	localStorage['Alarm departure setting']
+	localStorage['Alarm departure time']
+	localStorage['Alarm embark setting']
+	localStorage['Alarm embark time']
+	localStorage['Alarm alight setting']
+	localStorage['Alarm alight time']
+	*/
+};
