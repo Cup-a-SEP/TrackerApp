d5f446e6681ac60d9d9a222eaeaac15c8467c73b
unknown <s115869@S115869.campus.tue.nl>
2013-10-18 14:09:06 +0200

    Robust plan screen

--- www/js/misc.js
+++ www/js/misc.js
@@ -68,3 +68,30 @@ Misc.lDistance.DefaultCost =
 	ins: 1,
 	subs: 1
 };
+
+/**
+ * Splits the given time into the hour and minute part
+ * @param {String} time              - a time in a hh:mm format
+ * @returns an array of the form [hours, minutes]. Returns null if the time is not a valid time or not in the correct format
+ */
+Misc.splitTime = function MiscSplitTime(time){
+    var split = time.match(/^(\d+):(\d+)$/);
+    if(split == null) return null;
+    split.shift();
+    if(split[0] > 23 || split[1] > 59) return null;
+    return split;
+};
+
+/**
+ * Splits the given date into the day, month and year part
+ * @param {String} date             - a date in a yyyy:mm:dd format
+ * @returns an array of the form [day, month, year]. Returns null if the date is not in the correct format and if day > 31 or month > 12.
+ * Note that this function will accept impossible dates like for expample 2013-2-31
+ */
+Misc.splitDate = function MiscSplitDate(date){
+    var split = date.match(/^(\d+)-(\d+)-(\d+)$/);
+    if(split == null) return null;
+    split.shift();
+    if(split[1] > 12 || split[2] > 31) return null;
+    return split;
+};
