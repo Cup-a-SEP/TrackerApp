96e134ba263acc486a31770ff3b981f634df2aaa
Jeroen <jeroen@wlan-222146.nbw.tue.nl>
2013-11-21 19:09:58 +0100

    Improved commenting of js and moved some to a .js file out of .html.

--- www/js/init.js
+++ www/js/init.js
@@ -1,3 +1,79 @@
+document.addEventListener("deviceready", Loaded, false);
+
+function Loaded()
+{	
+	// Setup event handlers
+	document.addEventListener("backbutton", function() { Page.back(); }, false);
+	
+	// Upon exiting the app (to the home-screen, another app, etc)
+	document.addEventListener("pause", function()
+	{
+		
+		// Get next alarm time
+		var next = Service.Alarm.check();
+		
+		// Translate internal representations
+		var map = {'departure': 'Vertrek', 'embark': 'Instap', 'alight': 'Uitstap'};
+		
+		// Unless there is no alarm set, arm the background alarm service
+		if (next < Infinity) {
+			setBackgroundAlarm(next, 'Reis alarm!', 'De ' + map[Service.Alarm.type(next)] + 'wekker ging af!');
+		}
+	}, false);
+	
+	// Upon re-entering the app
+	document.addEventListener("resume", function()
+	{
+		// Re-calculate the alarms and cancel the background alarm in favor of the in-app alarm.
+		Service.Alarm.refresh();
+		cancelBackgroundAlarm();
+	}, false);
+	
+	// Initialize database/storage
+	Storage.init();
+	Storage.Locations.init();
+	Storage.Trips.init();
+				
+	// Setup header
+	$('#settingsButton').click(function()
+	{
+		Page.open('settings.html', Page.Settings);
+		return false;
+	});
+	
+	$('#feedbackButton').click(function()
+	{
+		Page.open('feedback.html', Page.Feedback);
+		return false;
+	});
+	
+	// Page loading
+	Page.Body = $('#section_container');
+
+	// Drop the UI below the statusBar on iOS7 (no Cordova device plugin loaded, so using th userAgent)
+	if (!!navigator.userAgent.match(/iPhone OS 7/i)) {
+		// both body and header need extra padding as the header has fixed position
+      		$('body, #header').css('padding-top', '20px');
+		}
+	
+	// TOGGLE THIS BEFORE RELEASE !!!TOGGLE THIS BEFORE RELEASE !!! TOGGLE THIS BEFORE RELEASE !!!  
+
+	/** /
+	Page.replace('debug.html');
+	/**/
+	if (localStorage['OTP data'])
+		Page.replace('currenttrip.html', Page.CurrentTrip);
+	else
+		Page.replace('plan.html', Page.Plan);
+	/**/
+}
+
+$(function()
+{
+	// Still load when not on phone for testing purposes
+	if (!navigator.app)
+		Loaded();
+});
 
 $(function()
 {
